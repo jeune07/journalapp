5fe71fe2b9c0e102e37a40de6768483859e1943c
jeune07 <jeunewinsley9@gmail.com>
2024-04-03 18:25:56 -0300

    connected to firebaseDB

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,22 @@
 import { IconButton } from '@mui/material';
 import { AddOutlined } from '@mui/icons-material';
+import {useDispatch} from  "react-redux"
 
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { startNewNote } from '../../store/journal/thunks';
 
 export const JournalPage = () => {
-  return (
+const dispatch  = useDispatch()
+
+
+const onClickNewNote = () => {
+  dispatch(startNewNote())
+}
+
+
+
+return (
     <JournalLayout>
       
       {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
@@ -15,6 +26,7 @@ export const JournalPage = () => {
 
 
       <IconButton
+      onClick={onClickNewNote}
         size='large'
         sx={{
           color: 'white',
